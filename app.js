2bd69edcd27e5197ae71adc64cfc1f9efc9ecf12
Trung Dao <trungdao912@gmail.com>
2018-10-26 09:15:18 +0700

    deploy

---
+++ app.js
@@ -0,0 +1,12 @@
+let express = require('express');
+let app = express();
+let port = process.env.port || 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.listen(port);
